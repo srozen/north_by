0ba61af90980c850bebde7551c3c59481cae2436
Samuel Monroe <spat.monroe@gmail.com>
2022-10-19 08:24:07 +0200

    Using Stimulus Value to hold state

--- app/javascript/controllers/favorite_toggle_controller.ts
+++ app/javascript/controllers/favorite_toggle_controller.ts
@@ -2,13 +2,37 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="favorite-toggle"
 export default class FavoriteToggleController extends Controller {
-  static targets = ["elementToHide"]
+  static targets = ["elementToHide", "hideButton"]
   elementToHideTarget: HTMLElement
+  hideButtonTarget: HTMLElement
+
+  static values = { visible: Boolean }
+  visibleValue: boolean
+
   connect(): void {
-    console.log("The controller is connected")
+    this.updateHiddenClass()
+    this.updateText()
   }
 
   toggle(): void {
-    this.elementToHideTarget.classList.toggle("hidden")
+    this.toggleState()
+    this.updateHiddenClass()
+    this.updateText()
+  }
+
+  updateHiddenClass(): void {
+    this.elementToHideTarget.classList.toggle("hidden", !this.visibleValue)
+  }
+
+  toggleState(): void {
+    this.visibleValue = !this.visibleValue
+  }
+
+  visibleText(): string {
+    return this.visibleValue ? "Hide" : "Show"
+  }
+
+  updateText(): void {
+    this.hideButtonTarget.innerText = this.visibleText()
   }
 }
