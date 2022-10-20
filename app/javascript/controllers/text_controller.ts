e23a68a35104bb1455697e44c0f8766bcd9b3652
Samuel Monroe <spat.monroe@gmail.com>
2022-10-20 13:31:53 +0200

    Generic Stimulus

---
+++ app/javascript/controllers/text_controller.ts
@@ -0,0 +1,37 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="text"
+export default class TextController extends Controller {
+  static targets = ["elementWithText"]
+  elementWithTextTarget: HTMLElement
+
+  static values = {
+    status: Boolean,
+    on: { type: String, default: "On" },
+    off: { type: String, default: "Off" },
+  }
+
+  offValue: string
+  onValue: string
+  statusValue: boolean
+
+  toggle(): void {
+    this.flipState()
+  }
+
+  flipState() {
+    this.statusValue = !this.statusValue
+  }
+
+  statusValueChanged(): void {
+    this.updateText()
+  }
+
+  newText(): string {
+    return this.statusValue ? this.onValue : this.offValue
+  }
+
+  updateText(): void {
+    this.elementWithTextTarget.innerText = this.newText()
+  }
+}
