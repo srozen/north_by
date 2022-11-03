448e4421d559f7743405aaf93b4e7d2372d0f9b9
Samuel Monroe <spat.monroe@gmail.com>
2022-11-03 07:08:00 +0100

    Animate turbo streams

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,7 +5,23 @@
  * courses, books, articles, and the like. Contact us if you are in doubt.
  * We make no guarantees that this code is fit for any purpose.
  * Visit https://pragprog.com/titles/nrclient2 for more book information.
-***/
+ ***/
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "./components/venue_display"
+import "animate.css"
+
+document.addEventListener("turbo:before-stream-render", (event) => {
+  console.log(event.target.action)
+  if (event.target.action === "remove") {
+    const targetFrame = document.getElementById(event.target.target)
+    if (targetFrame.dataset.animateOut) {
+      event.preventDefault()
+      const elementBeingAnimated = targetFrame
+      elementBeingAnimated.classList.add(targetFrame.dataset.animateOut)
+      elementBeingAnimated.addEventListener("animationend", () => {
+        targetFrame.remove()
+      })
+    }
+  }
+})
